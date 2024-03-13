0568dd7b0a8a0790478a0bc8b05d590baa7a5ca1
Ella <ellaaj.eaj@gmail.com>
2024-03-13 15:36:54 +0000

    index and base html updated

--- rate/urls.py
+++ rate/urls.py
@@ -5,4 +5,8 @@ app_name = 'rate'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('genres/', views.genres, name='genres'),
+    path('add_rating/', views.add_rating, name ='add_rating'),
+    path('my_media/', views.my_media, name='my_media'),
+    path('login/', views.login, name='login'),
 ]
