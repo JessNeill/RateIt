39ac23ec97c6f99bce87c6cc0cce93ae2e1beb0a
John Wang <2769792w@student.gla.ac.uk>
2024-03-19 16:56:04 +0000

    Login Page Fix

--- rate/models.py
+++ rate/models.py
@@ -6,14 +6,17 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class MyUserManager(BaseUserManager):
     def create_user(self, username, password=None, **extra_fields):
         if not username:
-            raise ValueError('Users must have an email address')
+            raise ValueError('The given username must be set')
+        
         username = self.normalize_email(username)
+        
+        if User.objects.filter(username=username).exists():
+            raise ValueError('A user with this username already exists')
+        
         user = self.model(username=username, **extra_fields)
-        user.email = username
         user.set_password(password)
         user.save(using=self._db)
         return user
-
 
     def create_superuser(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
@@ -31,7 +34,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     username = models.EmailField(max_length=255, unique=True)
-    email = models.EmailField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
@@ -41,7 +43,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
-        return self.email
+        return self.username
+
+
     
 class Movie(models.Model):
     movie_id = models.IntegerField(unique = True)
@@ -85,4 +89,4 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
     def __str__(self):
-        return self.user.email
+        return self.user.username
