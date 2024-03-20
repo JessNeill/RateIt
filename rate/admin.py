0adde1c3004e66b20a0444b414a9d741636e7856
Ella <ellaaj.eaj@gmail.com>
2024-03-20 11:56:43 +0000

    admin.py conflicts resolved

--- rate/admin.py
+++ rate/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-#from .models import User,
+from .models import User
 from .models import UserProfile
 from .models import Movie, Book
 from .models import Movie_Rating, Book_Rating
