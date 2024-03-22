2b8d6c879ab8f2fa752a93118041712140f7dae1
Maria Brzezinska <2775727b@student.gla.ac.uk>
2024-03-22 12:13:34 +0000

    Added Model testing

--- rate/tests.py
+++ rate/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from .models import Movie, Book, Movie_Rating, Book_Rating, UserProfile
-from .forms import UserForm, UserProfileForm, MovieRatingForm, BookRatingForm
+from rate.models import Movie, Book, Movie_Rating, Book_Rating, UserProfile
+from rate.forms import UserForm, UserProfileForm, MovieRatingForm, BookRatingForm
 
 
 # Form tests
@@ -43,3 +43,41 @@ class BookRatingFormTest(TestCase):
         form_data = {'book_id': self.book.book_id, 'rating': 4, 'comment': 'Interesting read.', 'title': 'Brave New World'}
         form = BookRatingForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+# Model tests
+User = get_user_model()
+
+class UserModelTest(TestCase):
+    def test_user_str(self):
+        user = User.objects.create_user(username='testuser@example.com', password='testpass123', first_name='Test', last_name='User')
+        self.assertEqual(str(user), 'testuser@exampe.com')
+
+class MovieModelTest(TestCase):
+    def test_movie_str(self):
+        movie = Movie.objects.create(movie_id=1, title='Inception', genre='Action')
+        self.assertEqual(str(movie), 'Inception')
+
+class BookModelTest(TestCase):
+    def test_book_str(self):
+        book = Book.objects.create(book_id=1, book_title='The Great Gatsby', genre='Fiction')
+        self.assertEqual(str(book), 'The Great Gatsby')
+
+class MovieRatingModelTest(TestCase):
+    def test_movie_rating_str(self):
+        user = User.objects.create_user(username='ratinguser@example.com', password='ratingpass123')
+        movie = Movie.objects.create(movie_id=2, title='Interstellar', genre='Sci-Fi')
+        movie_rating = Movie_Rating.objects.create(movie_rating_id=1, movie_id=movie, user=user, rating=5, comment='Great film!')
+        self.assertEqual(str(movie_rating), '1')
+
+class BookRatingModelTest(TestCase):
+    def test_book_rating_str(self):
+        user = User.objects.create_user(username='bookluvr123@email.com', password='ilovebooks456')
+        book = Book.objects.create(book_id=2, title='1984', genre='Dystopian')
+        book_rating = Book_Rating.objects.create(book_rating_id=1, book_id=book, user=user, rating=4, comment='Decent read')
+        self.assertEquals(str(book_rating), '1')
+
+class UserProfileModelTest(TestCase):
+    def test_user_profile_str(self):
+        user = User.objects.create_user(username='profileuser@email.com', password='profilepass123')
+        user_profile = UserProfile.objects.create(user=user)
+        self.assertEqual(str(user_profile), 'profileuser@email.com')
