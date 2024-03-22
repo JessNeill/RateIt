ccd7fffd715ee1fbd339cb53ccffe6cdbe332f2c
Maria Brzezinska <2775727b@student.gla.ac.uk>
2024-03-22 12:54:43 +0000

    Added Views testing

--- rate/tests.py
+++ rate/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from rate.models import Movie, Book, Movie_Rating, Book_Rating, UserProfile
 from rate.forms import UserForm, UserProfileForm, MovieRatingForm, BookRatingForm
+from django.urls import reverse
+from django.test import Client
 
 
 # Form tests
@@ -18,7 +20,7 @@ class UserFormTest(TestCase):
 
 class UserProfileFormTest(TestCase):
     def test_user_profile_form_valid(self):
-        form_data = {}  # Assuming UserProfileForm does not require any fields
+        form_data = {}  
         form = UserProfileForm(data=form_data)
         self.assertTrue(form.is_valid())
 
@@ -81,3 +83,84 @@ class UserProfileModelTest(TestCase):
         user = User.objects.create_user(username='profileuser@email.com', password='profilepass123')
         user_profile = UserProfile.objects.create(user=user)
         self.assertEqual(str(user_profile), 'profileuser@email.com')
+
+# View tests
+class IndexViewTest(TestCase):
+    def test_index_view(self):
+        response = self.client.get(reverse('rate:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'rate/index.html')
+
+class GenresViewTest(TestCase):
+    def test_genres_view(self):
+        response = self.client.get(reverse('rate:genres'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'rate/genres.html')
+
+class AddRatingViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser@email.com', password='testpass123')
+        self.client = Client()
+
+    def test_add_rating_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('rate:add_rating'))
+        self.assertRedirects(response, '/rate/login/?next=/rate/add_rating/')
+    
+    def test_add_rating_logged_in_renders_correct_template(self):
+        self.client.login(username='testuser@email.com', password='testpass123')
+        response = self.client.get(reverse('rate:add_rating'))
+        self.assertEqual(str(response.context['user']), 'testuser@email.com')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'rate/add_rating.html')
+
+class MyMediaViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser@email.com', password='testpass123')
+        self.client = Client()
+    
+    def test_my_media_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('rate:my_media'))
+        self.assertRedirects(response, '/rate/login/?next=/rate/my_media/')
+
+    def test_my_media_logged_in_renders_correct_template(self):
+        self.client.login(username='testuser@email.com', password='testpass123')
+        response = self.client.get(reverse('rate:my_media'))
+        self.assertEqual(str(response.context['user']), 'testuser@email.com')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'rate/my_media.html')
+
+class UserLoginLogoutTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser@email.com', password='testpass123')
+        self.client = Client()
+
+    def test_login_page(self):
+        response = self.client.get(reverse('rate:login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'rate/login.html')
+    
+    def test_login_logout_functionality(self):
+        login = self.client.login(username='testuser@email.com', password='testpass123')
+        self.assertTrue(login)
+        response = self.client.get(reverse('rate:logout'))
+        self.assertRedirects(response, reverse('rate:index'))
+
+class RegistationViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_register_view(self):
+        response = self.client.get(reverse('rate:register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'rate/register.html')
+    
+    def test_register_form_submission(self):
+        response = self.client.post(reverse('rate:register'), {
+        'first_name': 'Test',
+        'last_name': 'User',
+        'username': 'testuser@email.com',
+        'password': 'testpass123'
+        })
+
+        self.assertEqual(User.objects.count(), 1)
+        self.assertRedirects(response, reverse('rate:login'))
